65be54c4054db92e9112f5a1641797aef5f45771
politechnika90909 <132575029+politechnika90909@users.noreply.github.com>
2023-05-18 14:43:07 +0200

    Update program14.c

--- laboratorium7b/program14.c
+++ laboratorium7b/program14.c
@@ -10,38 +10,37 @@
 
 char napisy[3][80]={"Ha! Ha! Ha! To tylko sygnał powierzchowny!\n", "Tylko proces %d śmieje się sygnałowi %d prosto w twarz!\n", "Auć - to bolało!\n"};
 int nr_napisu=0;
-sigset_t *sygnaly;
+sigset_t sygnaly;
 
 
 void obsluga_sig_int(int sig) {
-	sigaddset(sygnaly, sig);
+	sigaddset(&sygnaly, sig);
 }
 
 
 void obsluga_sig_quit(int sig) {
-	sigaddset(sygnaly, sig);
+	sigaddset(&sygnaly, sig);
 }
 
-void dawne(int sig){
-	exit(0);
-}
 
 
 void obsluga_sig_alarm(int sig){
-	if(sigismember(sygnaly, 2)){
+	if(sigismember(&sygnaly, 2)){
 		printf("SIGINT\n");
 	}
-	if(sigismember(sygnaly, 3)){
+	if(sigismember(&sygnaly, 3)){
 		printf("SIGQUIT\n");
 	}
-	signal(SIGINT, dawne);
-	signal(SIGQUIT, dawne);
+	signal(SIGINT, SIG_DFL);
+	signal(SIGQUIT, SIG_DFL);
+    printf("sprawdzam\n");
 }
 
 int main(int argc, char** argv) {
-	
-	sigemptyset(sygnaly);
-	
+
+
+    sigemptyset(&sygnaly);
+
 	signal(SIGINT,obsluga_sig_int);
 	signal(SIGQUIT, obsluga_sig_quit);
 	signal(SIGALRM, obsluga_sig_alarm);
